2f9856662c8698a178cd42bf7505d7529cee80a9
so9to <so9to@so9tonoMacBook-Pro.local>
2021-06-05 05:26:30 +0900

    change output format

--- Encoder/HammingEncoder.go
+++ Encoder/HammingEncoder.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"reflect"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -42,14 +43,13 @@ func main() {
 	}
 	fmt.Println("----------------------------")
 	code_size := int(math.Pow(2, float64(n)))
-	c := make([]int, n)
+	c := make([]string, n)
 	s := make([]int, m_size)
+	C := 0
 	s0 := make([]int, m_size)
-	//count := 0
 	out_fl, e2 := os.Create("output.txt")
 	if e2 != nil {
-		fmt.Println(os.Stderr, e2)
-		os.Exit(1)
+		panic(e2)
 	}
 	defer out_fl.Close()
 	writer := bufio.NewWriter(out_fl)
@@ -58,29 +58,32 @@ func main() {
 		i_n = i
 		row = 0
 		s = make([]int, m_size)
+		for i := 0; i < n; i++ {
+			c[i] = "0"
+		}
 		for i_n != 0 { //符号語Cを作る
 			tmp = i_n % 2
 			i_n /= 2
-			c[row] = tmp
+			c[row] = strconv.Itoa(tmp)
 			row++
 		}
-		//fmt.Printf("%d | code is %d\n", i, c)
+		//fmt.Printf("%d | code is %s\n", i, c)
 		for i := 0; i < m_size; i++ { //シンドロームsをHc^t=sより作成
 			for j := 0; j < n; j++ {
-				s[i] += H[i][j] * c[j]
+				C, _ = strconv.Atoi(c[j])
+				s[i] += H[i][j] * C
 			}
 			s[i] %= 2
 			if s[i] != 0 {
 				continue
 			}
+			//S[i] = strconv.Itoa(s[i])
 		}
 		//fmt.Printf("%d | syndrome is %d\n", i, s)
 		if reflect.DeepEqual(s, s0) {
-			//fmt.Printf("%d code is %d\n", count, c)
-			//count++
-			if _, e3 := fmt.Fprintln(writer, c); e3 != nil {
-				fmt.Println(os.Stderr, e3)
-				os.Exit(1)
+			str := strings.Join(c, "")
+			if _, e3 := fmt.Fprintln(writer, str); e3 != nil {
+				panic(e3)
 			}
 		}
 	} //*/
---
+++ Encoder/replace.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	fp, e1 := os.Open("output.txt")
+	if e1 != nil {
+		panic(e1)
+	}
+	defer fp.Close()
+	b, e11 := ioutil.ReadAll(fp)
+	if e11 != nil {
+		panic(e1)
+	}
+	str := string(b)
+	// 普通に表示
+	fmt.Println(str)
+
+	//置換作業
+	str = strings.Replace(str, "[", "", -1)
+	str = strings.Replace(str, "]", "", -1)
+	str = strings.Replace(str, " ", "", -1)
+	fmt.Printf("replaced :\n%s\n", str)
+	out_fl, e2 := os.Create("output2.txt")
+	if e2 != nil {
+		panic(e2)
+	}
+	defer out_fl.Close()
+	writer := bufio.NewWriter(out_fl)
+	if _, e3 := fmt.Fprintln(writer, str); e3 != nil {
+		panic(e3)
+	}
+	writer.Flush()
+}
